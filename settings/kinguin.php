8c90f78d95282801c968286cd80bfab0c3d829d7
Rehan <rehanmasiya786@gmail.com>
2022-09-07 16:15:51 +0530

    .gitignore is now working

--- settings/kinguin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'API_KEY' => '1e28b09a31c12415296ec98214ef7887', //'1955f72c9e9c72f58551953b7d2da4ff',
-    'ENDPOINT' => 'https://gateway.sandbox.kinguin.net/esa/api' //'https://gateway.kinguin.net/esa/api',
-    
-];
