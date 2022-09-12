1b65b5408572bb8b6152dc4cebf59373d3232d79
Mahamad Ali <manknojiya121@gmail.com>
2022-09-12 12:35:16 +0530

    responsive

--- segments/views/app.jly.php
+++ segments/views/app.jly.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta content="Templines" name="author">
         <meta content="TeamHost" name="description">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="HandheldFriendly" content="true">
         <meta name="format-detection" content="telephone=no">
         <meta content="IE=edge" http-equiv="X-UA-Compatible">
--- segments/views/frontend/mb-search-area.jly.php
+++ segments/views/frontend/mb-search-area.jly.php
@@ -8,7 +8,7 @@
                 <option value="{{ $productPlatform['platform'] }}" {{ (isset($_GET["category"]) && $_GET["category"] == $productPlatform['platform']) ? 'selected' : '' }}>{{ $productPlatform['platform'] }}</option>
             @endforeach
             </select>
-            <div class="search__input"><i class="ico_search"></i><input type="search" class="search_name_mb" name="name" placeholder="{{ trans('store.search') }}" value="{{ $_GET['name'] ?? '' }}"></div>
+            <div class="search__input"><input type="search" class="search_name_mb" name="name" placeholder="{{ trans('store.search') }}" value="{{ $_GET['name'] ?? '' }}"></div>
             <div class="search__btn"><button type="submit"><i class="ico_search"></i></button></div>
         </div>
     </form>
