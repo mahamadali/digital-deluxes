5d3e1e1a6ff50b64dfccf03f0fa57cebc0d950cb
Mahamad Ali <manknojiya121@gmail.com>
2022-09-27 13:05:00 +0530

    national_id field added on checkout

--- segments/views/app.jly.php
+++ segments/views/app.jly.php
@@ -8,6 +8,11 @@
         <meta name="HandheldFriendly" content="true">
         <meta name="format-detection" content="telephone=no">
         <meta content="IE=edge" http-equiv="X-UA-Compatible">
+        <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+<meta property="og:type"               content="article" />
+<meta property="og:title"              content="When Great Minds Don’t Think Alike" />
+<meta property="og:description"        content="How much does culture influence creative thinking?" />
+<meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
         <meta name="token" content="{{ prevent_csrf_token() }}">
         <link rel="icon" type="image/x-icon" href="{{ url('assets/img/favicon.ico') }}">
         <link rel="shortcut icon" href="{{ url('assets/img/favicon.ico') }}" type="image/x-icon">
